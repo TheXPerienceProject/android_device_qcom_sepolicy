606a0463285c65566899101cb94d565d5975858d
klozz <carlosj@klozz.dev>
2023-04-03 04:09:33 +0000

    sepolicy-legacy: adapt to xperience
    Signed-off-by: klozz <carlosj@klozz.dev>

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -1,5 +1,5 @@
 # Board specific SELinux policy variable definitions
-SEPOLICY_PATH:= device/qcom/sepolicy-legacy-um
+SEPOLICY_PATH:= device/qcom/sepolicy-legacy
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS := \
     $(SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS) \
     $(SEPOLICY_PATH)/generic/public \
@@ -48,4 +48,4 @@ ifneq (,$(filter sdm660 msm8937 msm8953 msm8996 msm8998, $(TARGET_BOARD_PLATFORM
     endif
 endif
 
--include device/lineage/sepolicy/qcom/sepolicy.mk
+-include device/xperience/sepolicy/qcom/sepolicy.mk
